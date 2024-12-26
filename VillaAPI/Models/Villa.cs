ee82d465353cc2d477842e9d626b4a1c3732d046
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-26 21:14:07 +0200

    feat :Add  villa model

---
+++ VillaAPI/Models/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
