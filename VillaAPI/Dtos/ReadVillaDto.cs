1292ed70c7f6e98bccdec9e8b1e20a0fc570953a
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-26 21:13:44 +0200

    add read dto

---
+++ VillaAPI/Dtos/ReadVillaDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VillaAPI.Dtos
+{
+    public class ReadVillaDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string? Name { get; set; }
+
+    }
+}
