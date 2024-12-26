7fd5d9f60c90db39defb4d378d5b6a46b8798c2f
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-26 21:13:31 +0200

    feat :add on the fly datastore temporarly data store insted of data base for testing

---
+++ VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,17 @@
+﻿using VillaAPI.Dtos;
+using VillaAPI.Models;
+
+namespace VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<ReadVillaDto> VillaList = new List<ReadVillaDto> 
+        {
+            new ReadVillaDto { Id = 1 ,Name = "MARASI"},
+            new ReadVillaDto {Id = 2 ,Name = "SHARM"}
+        };
+
+    
+    }
+}
+ 
