a9f8aa05eee4692b20924edcf9614905ad67f3bd
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-28 18:13:45 +0200

    feat:Setup Database

---
+++ VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VillaAPI.Models;
+
+namespace VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) { }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Villa>()
+                        .HasData(
+              new Villa
+                       {
+                           Id = 1,
+                 Name = "Royal Villa",
+                 Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                 ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg",
+                 Occupancy = 4,
+                 Rate = 200,
+                 Sqft = 550,
+                 Amentiy = "",
+                 CraetedDtae = DateTime.Now,
+              },
+              new Villa
+              {
+                  Id = 2,
+                  Name = "Premium Pool Villa",
+                  Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg",
+                  Occupancy = 4,
+                  Rate = 300,
+                  Sqft = 550,
+                  Amentiy = "",
+                  CraetedDtae = DateTime.Now,
+              },
+              new Villa
+              {
+                  Id = 3,
+                  Name = "Luxury Pool Villa",
+                  Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg",
+                  Occupancy = 4,
+                  Rate = 400,
+                  Sqft = 750,
+                  Amentiy = "",
+                  CraetedDtae = DateTime.Now,
+              }
+                );
+        }
+    }
+}
