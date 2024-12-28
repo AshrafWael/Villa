d415e2cf9aaf4152c51c28bea8a5caa1e63c4a19
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-28 18:14:47 +0200

    feat: Setup AutoMapper

---
+++ VillaAPI/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using VillaAPI.Dtos;
+using VillaAPI.Models;
+
+namespace VillaAPI.Mapping
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Villa,AddVillaDto>().ReverseMap();
+            CreateMap<Villa, UpdateVillaDto>().ReverseMap();
+            CreateMap<Villa, ReadVillaDto>().ReverseMap();
+
+        }
+    }
+}
