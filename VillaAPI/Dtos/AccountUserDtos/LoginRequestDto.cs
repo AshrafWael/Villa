3707b40a0814dc2bb7aff3fc285cb96a906f8265
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 14:24:42 +0200

    user dtos

---
+++ VillaAPI/Dtos/AccountUserDtos/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VillaAPI.Dtos.AccountUserDtos
+{
+    public class LoginRequestDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ VillaAPI/Dtos/AccountUserDtos/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using VillaAPI.Models;
+
+namespace VillaAPI.Dtos.AccountUserDtos
+{
+    public class LoginResponseDto
+    {
+       public User User { get; set; }
+        public string Token { get; set; }
+
+    }
+}
