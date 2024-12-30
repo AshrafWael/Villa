e683c48b4f4d387e51f5207ba0379a5d5fbb5b54
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 04:12:02 +0200

    Villa Number Dtos

---
+++ VillaAPI/Dtos/VillaNumberDtos/AddVillaNumberDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VillaAPI.Dtos.VillaNumberDtos
+{
+    public class AddVillaNumberDto
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public int VillaId { get; set; }
+
+    }
+}
---
+++ VillaAPI/Dtos/VillaNumberDtos/UpdateVillaNumberDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VillaAPI.Dtos.VillaNumberDtos
+{
+    public class UpdateVillaNumberDto
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public int VillaId { get; set; }
+    }
+}
