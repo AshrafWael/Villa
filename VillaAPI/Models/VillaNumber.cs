f670b66f86a2d52638ed9e009c97b1ea98f133c4
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 04:13:30 +0200

    add vill number Proprty

---
+++ VillaAPI/Models/VillaNumber.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VillaAPI.Models
+{
+    public class VillaNumber
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int VillaNo { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        [ForeignKey("Villa")]
+        public int VillaId { get; set; }
+        public Villa Villa { get; set; }
+    }
+}
