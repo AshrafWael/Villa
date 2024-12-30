610408e0a61bc9c383c30b7726f8b7fca6087ca5
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 02:08:15 +0200

    generic repo interface

---
+++ VillaAPI/IRepository/IVillaRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using VillaAPI.Dtos;
+using VillaAPI.Models;
+
+namespace VillaAPI.IRepository
+{
+    public interface IVillaRepository : IGenericRepository<Villa> 
+    {
+        Task<Villa> UpdateAsync(Villa entity);
+    }
+}
