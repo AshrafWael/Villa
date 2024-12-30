f9b9ef364bab90e12053f78b58b8664b78542528
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 04:14:17 +0200

    villa number repository implmentation

---
+++ VillaAPI/Repository/VillaNumberRepository.cs
@@ -0,0 +1,23 @@
+﻿using VillaAPI.Data;
+using VillaAPI.IRepository;
+using VillaAPI.Models;
+
+namespace VillaAPI.Repository
+{
+    public class VillaNumberRepository : GenericRepository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaNumberRepository(ApplicationDbContext dbContext) : base(dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<VillaNumber> UpdateAsync(VillaNumber entity)
+        {
+            entity.UpdatedDate = DateTime.Now;
+            _dbContext.villaNumbers.Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
