213995d589df03f2e2321edcb7a7467cc43d4495
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 02:07:19 +0200

    add generic repo

---
+++ VillaAPI/Repository/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VillaAPI.Data;
+using VillaAPI.IRepository;
+
+namespace VillaAPI.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> _dbset;
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbset = _dbContext.Set<T>();   
+        }
+        public async Task<IEnumerable<T>> GetAllAsync(System.Linq.Expressions.Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = _dbset;
+            if (filter != null) 
+            {
+              query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+        public async Task<T> GetAsync(System.Linq.Expressions.Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbset;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await _dbset.AddAsync(entity);
+           await SaveAsync();
+        }
+        public async Task RemoveAsync(T entity)
+        {
+            _dbset.Remove(entity);
+            await SaveAsync();
+        }
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using VillaAPI.Data;
+using VillaAPI.Dtos;
+using VillaAPI.IRepository;
+using VillaAPI.Models;
+
+namespace VillaAPI.Repository
+{
+    public class VillaRepository : GenericRepository<Villa> ,IVillaRepository 
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaRepository(ApplicationDbContext dbContext) :base(dbContext) 
+        { 
+            _dbContext = dbContext;
+        }
+        public async Task<Villa> UpdateAsync(Villa entity)
+        {
+            entity.UpdatededDtae = DateTime.Now;
+            _dbContext.Villas.Update(entity);
+           await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
