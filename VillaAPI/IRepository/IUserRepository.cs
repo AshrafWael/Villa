762ad5bd50aede9bc40ac91a626f52421d755ffd
Ashraf Wael <ashrafwael582@gmail.com>
2024-12-30 14:24:56 +0200

    user repostory interface

---
+++ VillaAPI/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using VillaAPI.Dtos.AccountUserDtos;
+using VillaAPI.Models;
+
+namespace VillaAPI.IRepository
+{
+    public interface IUserRepository
+    {
+        public bool IsUniqueUser(string username);
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        public Task<User> Register(RegisterRequestDto registerRequestDto);
+    }
+}
