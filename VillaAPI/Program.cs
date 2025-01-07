b25d50b351397df31663db7d9f60a17580ed046c
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-07 23:39:40 +0200

    Add JWT Configration and Registration

--- VillaAPI/Program.cs
+++ VillaAPI/Program.cs
@@ -1,9 +1,15 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using VillaAPI.Data;
 using VillaAPI.IRepository;
 using VillaAPI.Mapping;
+using VillaAPI.Models;
 using VillaAPI.Repository;
-
+using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -17,8 +23,112 @@ builder.Services.AddScoped<IVillaRepository,VillaRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(options=>
+{ 
+options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1,0);
+    options.ReportApiVersions = true;
+
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+
+});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "My API v1",
+        Version = "v1.0" ,
+        Description = "Api to Vilaa",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contact Me",
+            Url = new Uri("https://example.com/terms"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Exaple License",
+            Url = new Uri("https://example.com/terms")
+        }
+
+    });
+    c.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Title = "My API v2",
+        Version = "v2.0",
+        Description = "Api to Vilaa version 2",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contact Me",
+            Url = new Uri("https://example.com/terms"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Exaple License",
+            Url = new Uri("https://example.com/terms")
+        }
+
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your token in the text input below.\n\nExample: `Bearer abc123`"
+    });
+    // Add a global security requirement
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+                //Scheme = "oauth2",
+                //         Name= "Bearer",
+                //         In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:SecretKey");
+builder.Services.AddAuthentication(a =>
+  {
+      a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  }).AddJwtBearer(x=> 
+  { 
+      x.RequireHttpsMetadata = false;
+      x.SaveToken=true;
+      x.TokenValidationParameters = new TokenValidationParameters
+      {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+          ValidateIssuer = false,
+          ValidateAudience = false
+      };
+      
+      
+  });
+
+
 
 var app = builder.Build();
 
@@ -26,13 +136,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options=>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json","VillaAPI_V1");
+        options.SwaggerEndpoint("/swagger/v2/swagger.json", "VillaAPI_V2");
+
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
