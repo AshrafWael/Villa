8463efe54968e0e60ac592245fc011fb58b1f520
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-07 23:38:46 +0200

    Updete User Repo

--- VillaAPI/Repository/UserRepository.cs
+++ VillaAPI/Repository/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using VillaAPI.Data;
@@ -13,16 +15,23 @@ namespace VillaAPI.Repository
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private string secretkey;
 
-        public UserRepository(ApplicationDbContext dbContext ,IConfiguration configuration)
+        public UserRepository(ApplicationDbContext dbContext ,IConfiguration configuration,
+            UserManager<ApplicationUser> userManager,IMapper mapper,RoleManager<IdentityRole> roleManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
+            _mapper = mapper;
+            _roleManager = roleManager;
             secretkey = configuration.GetValue<string>("ApiSettings:SecretKey")!;
         }
         public bool IsUniqueUser(string username)
         {
-             var user = _dbContext.Users.FirstOrDefault(u =>u.UserName == username);
+             var user = _dbContext.ApplicationUsers.FirstOrDefault(u =>u.UserName == username);
             if (user == null) 
             {
             return true;
@@ -33,9 +42,12 @@ namespace VillaAPI.Repository
 
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
-            var user = await _dbContext.Users.FirstOrDefaultAsync(u=> u .UserName.ToLower() ==
-            loginRequestDto.UserName.ToLower() && u.Password == loginRequestDto.Password);
-            if (user == null) 
+
+            var user = await _dbContext.ApplicationUsers.FirstOrDefaultAsync(u=>
+               u.Email ==loginRequestDto.Email || u.UserName.ToUpper() == loginRequestDto.Username.ToUpper());
+            var email = _userManager.GetEmailAsync(user);
+            bool IsValid = await _userManager.CheckPasswordAsync(user,loginRequestDto.Password);
+            if (user == null || IsValid == false) 
             {
                 return new LoginResponseDto()
                 {
@@ -45,6 +57,8 @@ namespace VillaAPI.Repository
             
             }
             //generate token 
+
+            var Roles = await _userManager.GetRolesAsync(user);
             var tokenhandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(secretkey);
 
@@ -52,8 +66,8 @@ namespace VillaAPI.Repository
             {
                 Subject = new ClaimsIdentity(new Claim[]
                {
-                new Claim(ClaimTypes.Name,user.Id.ToString()),
-                new Claim(ClaimTypes.Role,user.Role)
+                new Claim(ClaimTypes.Name,user.Email.ToString()),
+                new Claim(ClaimTypes.Role,Roles.FirstOrDefault())
 
                }),
                 Expires = DateTime.UtcNow.AddDays(7),
@@ -64,25 +78,41 @@ namespace VillaAPI.Repository
             {
 
                 Token = tokenhandler.WriteToken(token),
-                User = user
+                User = _mapper.Map<ApplicationUserDto>(user),
+               // Role = Roles.FirstOrDefault(),
             };
             return loginResponse;
 
         }
 
-        public async Task<User> Register(RegisterRequestDto registerRequestDto)
+        public async Task<ApplicationUserDto> Register(RegisterRequestDto registerRequestDto)
         {
-            User user = new()
+            ApplicationUser user = new()
             {
+                Email = registerRequestDto.Email,
+                NormalizedEmail= registerRequestDto.Email.ToUpper(),
                 UserName =registerRequestDto.UserName,
                 Name = registerRequestDto.Name,
-                Role = registerRequestDto.Role,
-                Password = registerRequestDto.Password,
-            };
-            _dbContext.Users.Add(user);
-            await _dbContext.SaveChangesAsync();
-            user.Password = "";
-            return user;
+             };
+            try
+            {
+                var result = await _userManager.CreateAsync(user,registerRequestDto.Password);
+                if (result.Succeeded) 
+                {
+                    if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                    }
+                    await _userManager.AddToRoleAsync(user, "admin");
+                        var usertoreturn =await _dbContext.ApplicationUsers
+                        .FirstOrDefaultAsync(u =>
+                        u.Email == registerRequestDto.Email );
+                    return _mapper.Map<ApplicationUserDto>(usertoreturn);
+                }
+            }
+            catch (Exception ex)
+            { }
+            return new ApplicationUserDto();
             
         }
     }
