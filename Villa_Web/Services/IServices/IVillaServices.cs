98f4234d717ee01d9741fc96aad30bc71878d521
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-13 23:16:56 +0200

    Add villa Service Declaration

---
+++ Villa_Web/Services/IServices/IVillaServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using Villa_Web.Dtos.VillaDtos;
+
+namespace Villa_Web.Services.IServices
+{
+    public interface IVillaServices
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task CreateAsync<T>( AddVillaDto villaDto);
+        Task UpdateAsync<T>(UpdateVillaDto updateVillaDto );
+        Task DeleteAsync<T>(int id);
+
+    }
+}
