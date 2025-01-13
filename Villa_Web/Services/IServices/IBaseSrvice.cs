c35eb29f3b69650537dad9d5a68b3e76d84d58eb
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-13 23:16:22 +0200

    Add Send Requst Function

---
+++ Villa_Web/Services/IServices/IBaseSrvice.cs
@@ -0,0 +1,12 @@
+﻿using Villa_Web.Models;
+using Villa_Web.Responses;
+
+namespace Villa_Web.Services.IServices
+{
+    public interface IBaseSrvice
+    {
+        APIResponse ResponceModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apirequest);  
+          
+    }
+}
