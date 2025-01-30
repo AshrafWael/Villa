41495af9176ed2651d816f5fd901bea0fa3b8ccd
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-30 11:10:17 +0200

    add Create and Update Villa End Point

--- Villa_Web/Controllers/VillaController.cs
+++ Villa_Web/Controllers/VillaController.cs
@@ -1,7 +1,9 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Villa_Web.Dtos.VillaDtos;
+using Villa_Web.Models;
 using Villa_Web.Responses;
 using Villa_Web.Services.IServices;
 
@@ -17,16 +19,60 @@ namespace Villa_Web.Controllers
             _villaServices = villaServices;
             _Mapper = mapper;
         }
-
         public async Task<IActionResult> IndexVilla()
         {
             List<ReadVillaDto> list = new();
             var response = await _villaServices.GetAllAsync<APIResponse>();
-            if (response != null && response.IsSuccess) 
+            if (response != null && response.IsSuccess)
             {
-            list = JsonConvert.DeserializeObject<List<ReadVillaDto>>(Convert.ToString(response.Result)!)!;
+                list = JsonConvert.DeserializeObject<List<ReadVillaDto>>(Convert.ToString(response.Result)!)!;
             }
             return View(list);
+         }
+        public async Task<IActionResult> CreateVilla()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(AddVillaDto villadto)
+        {
+            if (ModelState.IsValid) 
+            {
+            var villa = await _villaServices.CreateAsync<APIResponse>(villadto);
+                if (villa != null && villa.IsSuccess) 
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+             }
+            return View(villadto);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> UpdateVilla(int id)
+        {
+            var villa = await _villaServices.GetAsync<APIResponse>(id);
+            if (villa != null && villa.IsSuccess)
+            {
+                var model = JsonConvert.DeserializeObject<Villa>(Convert.ToString(villa.Result)!)!;
+                var mappedvilla = _Mapper.Map<UpdateVillaDto>(model);
+                return View(mappedvilla);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateVilla(UpdateVillaDto villaDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                var response = await _villaServices.UpdateAsync<APIResponse>(villaDto);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(villaDto);
         }
     }
 }
