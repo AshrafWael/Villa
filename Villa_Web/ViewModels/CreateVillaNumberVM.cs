9d1331a053c98edfb6d2124ffa321ac3e8b08a00
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-23 17:11:11 +0200

    Add Vill Number View Models

---
+++ Villa_Web/ViewModels/CreateVillaNumberVM.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Villa_Web.Dtos.VillaNumberDtos;
+
+namespace Villa_Web.ViewModels
+{
+    public class CreateVillaNumberVM
+    {
+
+        public CreateVillaNumberVM()
+        {
+            VillaNumber = new AddVillaNumberDto();
+        }
+        public AddVillaNumberDto  VillaNumber { get; set; }
+        [ValidateNever ]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
---
+++ Villa_Web/ViewModels/UpdateVillaNumberVM.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Villa_Web.Dtos.VillaNumberDtos;
+
+namespace Villa_Web.ViewModels
+{
+    public class UpdateVillaNumberVM
+    {
+
+        public UpdateVillaNumberVM()
+        {
+            VillaNumber = new UpdateVillaNumberDto();
+        }
+        public UpdateVillaNumberDto  VillaNumber { get; set; }
+        [ValidateNever ]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
