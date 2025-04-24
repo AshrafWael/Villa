474bdcab155d3555e3b3b044ce5917cdc13594d7
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-24 19:04:15 +0200

    Update Services

---
+++ Villa_Web/Services/IServices/IAuthServices.cs
@@ -0,0 +1,12 @@
+﻿using Villa_Web.Dtos.AccountUserDtos;
+
+namespace Villa_Web.Services.IServices
+{
+    public interface IAuthServices
+    {
+        Task<T> LoginAsync<T>(LoginRequestDto logindto);
+        Task<T> RegisterAsync<T>(RegisterRequestDto registerdto);
+
+
+    }
+}
---
+++ Villa_Web/Services/IServices/IVillaNumberServices.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Villa_Web.Dtos.VillaDtos;
+using Villa_Web.Dtos.VillaNumberDtos;
+
+namespace Villa_Web.Services.IServices
+{
+    public interface IVillaNumberServices
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task<T> CreateAsync<T>( AddVillaNumberDto villaDto);
+        Task<T> UpdateAsync<T>(UpdateVillaNumberDto updateVillaDto );
+        Task<T> DeleteAsync<T>(int id);
+
+    }
+}
