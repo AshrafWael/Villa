bcbb3a585982d795024a10eb4cbbd3c6593155f5
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-26 21:40:57 +0300

    Update Home Page Index endpoint

--- Villa_Web/Controllers/HomeController.cs
+++ Villa_Web/Controllers/HomeController.cs
@@ -1,32 +1,41 @@
 using System.Diagnostics;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Villa_Web.Dtos.VillaDtos;
 using Villa_Web.Models;
+using Villa_Web.Responses;
+using Villa_Web.Services.IServices;
 
 namespace Villa_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IVillaServices _villaServices;
+        private readonly IMapper _Mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IVillaServices villaServices, IMapper mapper)
         {
-            _logger = logger;
+            _villaServices = villaServices;
+            _Mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ReadVillaDto> list = new();
+            var response = await _villaServices.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ReadVillaDto>>(Convert.ToString(response.Result)!)!;
+            }
+            return View(list);
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+     
 
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
