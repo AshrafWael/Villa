d49a2210bba3583d7f8360e573e8571b1dafffcf
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-26 21:41:52 +0300

    Update with Registration

--- Villa_Web/Program.cs
+++ Villa_Web/Program.cs
@@ -6,14 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 builder.Services.AddHttpClient<IVillaServices,VillaServices>();
 builder.Services.AddScoped<IVillaServices, VillaServices>();
 
+builder.Services.AddHttpClient<IVillaNumberServices, VillaNumberServices>();
+builder.Services.AddScoped<IVillaNumberServices, VillaNumberServices>();
 
+builder.Services.AddHttpClient<IAuthServices, AuthServices>();
+builder.Services.AddScoped<IAuthServices, AuthServices>();
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(Options =>
+{
+    Options.IdleTimeout = TimeSpan.FromMinutes(100);
+    Options.Cookie.HttpOnly = true;
+    Options.Cookie.IsEssential = true;
+}
+);
+builder.Services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -28,7 +43,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
